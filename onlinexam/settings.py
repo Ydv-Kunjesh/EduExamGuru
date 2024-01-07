3ea2fc813dbd104b18c67f6c5e8da13c2471a9c5
KUNJESH YDV <rajanshu23187@gmail.com>
2024-01-07 21:34:04 +0530

    Update settings.py

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
